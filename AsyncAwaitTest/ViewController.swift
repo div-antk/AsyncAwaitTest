b4b0a00dbbe6f442d82b303b28a2ea44a95489fd
div-antk <51283468+owaii@users.noreply.github.com>
2021-11-13 23:32:07 +0900

    UIの設置

--- AsyncAwaitTest/ViewController.swift
+++ AsyncAwaitTest/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var countUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
